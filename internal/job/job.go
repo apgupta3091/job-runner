069d41e87aa789ece7cb75c6a637662bf58a9b87
Anuj Gupta <apgupta3091@gmail.com>
2026-02-12 15:45:14 -0500

    add pointers to started at and completed at

--- internal/job/job.go
+++ internal/job/job.go
@@ -12,11 +12,11 @@ const (
 
 // Job represents a unit of work to be processed
 type Job struct {
-	ID          string      `json:"id"`
-	Payload     interface{} `json:"payload"`
-	Status      string      `json:"status"`
-	Error       string      `json:"error,omitempty"`
-	CreatedAt   time.Time   `json:"created_at"`
-	StartedAt   time.Time   `json:"started_at,omitempty"`
-	CompletedAt time.Time   `json:"completed_at,omitempty"`
+	ID          string     `json:"id"`
+	Payload     any        `json:"payload"`
+	Status      string     `json:"status"`
+	Error       string     `json:"error,omitempty"`
+	CreatedAt   time.Time  `json:"created_at"`
+	StartedAt   *time.Time `json:"started_at,omitempty"`
+	CompletedAt *time.Time `json:"completed_at,omitempty"`
 }
